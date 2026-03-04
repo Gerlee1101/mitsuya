43129d746abc7596a7c8d06f889072dcf55ac075
Gerlee1101 <gerelpinecone@gmail.com>
2026-03-04 16:17:09 +0800

    carousel yes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { Ceo } from "@/_component/Ceo";
 
 export default function Home() {
   return (
-    <main className="relative min-h-screen">
+    <main className="relative min-h-screen pt-1.5 md:pt-0 pb-12 md:pb-16">
       <div className="pt-16 md:pt-20 pb-12 md:pb-16">
         <CoverCarousel />
         <HomePage />
